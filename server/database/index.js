6d2225475251951ed8cc1afaae2bdaee376dddb5
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-06-29 17:50:30 -0700

    Created initial server structure

---
+++ server/database/index.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+    // TODO: Perform database setup here (db connection and models)
+};
---
+++ server/routes/index.js
@@ -0,0 +1,3 @@
+module.exports = function(app) {
+    // TODO: Implement handling api routes here (randomizer and user routes)
+};
