b3396b894e94611c16e29858c8362690b1f6613f
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-06-29 17:53:51 -0700

    Added server.js boilerplate

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+// Initialize Express app
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Initialize API routes
+const routes = require("./server/routes");
+routes(app);
+
+// TODO: Initialize Apollo Server for GraphQL
+
+// Default React route
+app.get("/*", (_, response) => {
+    response.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    if (process.env.NODE_ENV === "development") {
+        console.log(`Server is running on http://localhost:${PORT}/`);
+    }
+});
