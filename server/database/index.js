c9ea1ea880f1710a257f972f184ccb1e5a6a8d28
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-01 06:56:26 -0700

    Changed database name

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     const mongoose = require("mongoose");
-    const MONGO_DB = process.env.MONGODB_URI || "mongodb://localhost/paperbook";
+    const MONGO_DB = process.env.MONGODB_URI || "mongodb://localhost/fortherandomizing";
 
     // Initialize MongoDB database using Mongoose
     mongoose.connect(MONGO_DB, { useNewUrlParser: true });
