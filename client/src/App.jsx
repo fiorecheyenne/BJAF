46ca70303dff530e3e50f5f92a2e77281523cc55
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-02 21:22:28 -0700

    Added min-height rule to React root div

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+
+// Core application components
+import Nav from "./shared/Nav";
+import Footer from "./shared/Footer";
+import SplashPage from "./containers/SplashPage";
 import Favorites from "./Favorites";
 import User from "./User";
-import Index from "./Index";
 import Randomizer from "./Randomizer";
-import Nav from "./Nav";
 import ErrorPage from "./ErrorPage";
 
-function App() {
+export default function App() {
     return (
         <Router>
-            <div>
-                <Nav />
-                <Route exact path="/" component={Index} />
-                <Route exact path="/user/:id" component={User} />
-                <Route exact path="/user/:id/favorites" component={Favorites} />
-                <Route exact path="/error" component={ErrorPage} />
-                <Route exact path="/randomizer" component={Randomizer} />
-            </div>
+            <Nav />
+            <Switch>
+                <Route exact path="/" component={SplashPage} />
+                <Route path="/user/:id" component={User} />
+                <Route path="/user/:id/favorites" component={Favorites} />
+                <Route path="/error" component={ErrorPage} />
+                <Route path="/randomizer" component={Randomizer} />
+            </Switch>
+            <Footer />
         </Router>
     );
 }
