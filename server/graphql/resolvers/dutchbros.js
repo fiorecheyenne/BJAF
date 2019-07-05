4ccc3021c400661f7c5b0f00c7421157b4fb5f2a
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-05 14:22:21 -0700

    Added graphql api structure

---
+++ server/graphql/resolvers/dutchbros.js
@@ -0,0 +1,10 @@
+const data = require("../../../seed.json");
+
+module.exports = {
+    allOptions() {
+        // TODO: Return all avaliable options
+    },
+    randomizedOption() {
+        // TODO: Return generated randomized option
+    },
+};
