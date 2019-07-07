1c3cdf952b13f95fc9dbeb7f11f5b83a856f9bf7
Juan Palomino <Jpalomino09@gmail.com>
2019-07-07 14:12:43 -0700

    password hashing for create user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 // const passport = require("passport");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const User = require("../database/models/User.js");
 
 // Handles the GET, POST, PUT, and DELETE routes for users
@@ -22,24 +22,22 @@ module.exports = {
     // throw new Error("/api/user POST route not implemented yet.");
 
     POST: async function(request, response) {
-        // TODO: Implement posting new users
+
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(request.body.password, salt);
         var userData = request.body;
-        var newUser = await User.create(userData);
+        var newUser = await User.create({
+            email: userData.email,
+            username: userData.username,
+            password: hash,
+            faves: userData.faves,
+        });
         response.send(newUser);
-        // newUser
-        //     .then(User => {
-        //         console.log("dotthen");
-        //         res.json(User);
-        //     })
-        //     .catch(err => {
-        //         console.log("dotcatch");
-        //         response.status(400).send(err);
-        //     });
-        // response.send(newUser);
     },
 
     PUT: function(request, response) {
         // TODO: Implement updating users info
+
         throw new Error("/api/user PUT route not implemented yet.");
     },
     DELETE: function(request, response) {
