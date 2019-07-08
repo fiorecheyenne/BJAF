68a0555a1ce137abaac0ec4943e4c79e57e9ae96
Lexi127 <aperalta127@gmail.com>
2019-07-07 19:39:37 -0700

    accepted changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 const randomizer = require("./randomizer");
 const users = require("./user");
-<<<<<<< HEAD
-const auth = require("./auth");
-=======
 const login = require("./login");
->>>>>>> 52c1b96a4375f435b6cfc5e2eaae8ac5984c7a53
 
 module.exports = function(app) {
     app.get("/api/randomizer", randomizer.GET);
