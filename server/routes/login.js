83d556794db97a6c3384f46705025bbbe3f4289e
Juan Palomino <Jpalomino09@gmail.com>
2019-07-07 20:12:20 -0700

    return user in login post request

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,7 +2,15 @@ const User = require("../database/models/User.js");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-    POST: function (request, response) {
-        
-    }
-}
+    POST: async function(request, response) {
+        //LOGIN the user
+        // find a user by username
+        // pull the hashed password from db
+        // boolean = bcrypt.compareSync(INPUT PASS, HASHED PASS FROM DB)
+        // if (boolean){return token}
+        // if (!boolean){DONT WORRY ABOUT IT}
+
+        let loginUser = await User.findOne({username:request.body.username});
+        response.json(loginUser);
+    },
+};
--- server/tokenizer.js
+++ server/tokenizer.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const tokenKey = process.env.TOKEN_KEY;
 
 module.exports = function signing(user) {
-    let token = jwt.sign({...user}, tokenKey);
+    let token = jwt.sign(user._id.toString(), tokenKey);
     return {
         username: user.username,
         email: user.email,
