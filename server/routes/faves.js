33291355f81f92ce5347403507c403453a45b27d
Juan Palomino <Jpalomino09@gmail.com>
2019-07-10 15:59:11 -0700

    finished error handling for put and delete routes without try bl;ock

--- server/routes/faves.js
+++ server/routes/faves.js
@@ -4,7 +4,7 @@ const decoder = require("../decoder");
 const tokenKey = process.env.TOKEN_KEY;
 
 module.exports = {
-    PUT: async function(request, response, status) {
+    PUT: async function(request, response) {
         let item = request.body.item;
         // receive token
         let userToke = request.get("Auth-Token");
@@ -19,21 +19,24 @@ module.exports = {
             { $addToSet: { faves: item } }
         );
         let isAdded = updatedFaves.nModified;
+        if (!item || !item.name) {
+            console.log("item dont exist");
+            response.send({ status: 500, message: "Your fave needs a name before you save it! get creative!" });
+            return;
+        }
         if (isAdded) {
             console.log({ status: 200, Message: item + " Added to faves." });
             response.send({ status: 200, Message: item + " Added to faves." });
+            return;
         }
         if (!isAdded) {
             console.log({ status: 500, Message: item + " could not be added to faves because it already exists in array." });
             response.send({ status: 500, Message: item + " is already in your faves." });
+            return;
         }
     },
+
     DELETE: async function(request, response) {
-        try {
-            if (!request) throw "new err";
-        } catch (err) {
-            console.log("input");
-        }
         let item = request.body.item;
         // receive token
         let userToke = request.get("Auth-Token");
@@ -47,15 +50,28 @@ module.exports = {
             { $pull: { faves: item } }
         );
         let isAdded = pullFave.nModified;
-        console.log(pullFave);
 
+        class FaveResult {
+            constructor(stat, msg){
+                this.status = stat;
+                this.message = msg;
+            }
+        }
+
+        if (!item || !item.name) {
+            console.log("item dont exist");
+            response.send({ status: 500, message: "invalid input" });
+            return;
+        }
         if (isAdded) {
-            console.log({ status: 200, Message: item + " Deleted from faves." });
-            response.send({ status: 200, Message: item + " Deleted from faves." });
+            console.log({ status: 200, message: item + " deleted from faves." });
+            response.send({ status: 200, message: item + " deleted from faves." });
+            return;
         }
         if (!isAdded) {
-            console.log({ status: 500, Message: item + " could not be deleted from faves because does not exist in array." });
-            response.send({ status: 500, Message: item + " does not exist in faves." });
+            console.log({ status: 500, message: item + " could not be deleted from faves because does not exist in array." });
+            response.send({ status: 500, message: "'" + item.name + "' does not exist in faves." });
+            return;
         }
     },
 };
