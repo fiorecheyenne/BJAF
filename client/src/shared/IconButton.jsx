fe68b7bb99086f8fa92ebeb9d25a69d4cb630578
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-11 20:45:55 -0700

    Added more customization to IconButton

--- client/src/shared/IconButton.jsx
+++ client/src/shared/IconButton.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function IconButton({ icon, iconGroup = "fas", buttonSize, iconSize, onClick }) {
+export default function IconButton({ icon, iconGroup = "fas", buttonSize, iconSize, isRound, extras, onClick }) {
     return (
         <div className="field has-addons">
             <p className="control">
-                <button type="button" onClick={onClick} className={`button ${buttonSize && "is-" + buttonSize}`}>
+                <button
+                    type="button"
+                    onClick={onClick}
+                    className={`button${buttonSize ? " is-" + buttonSize : ""}${isRound ? " is-rounded" : ""}${
+                        extras ? " " + extras : ""
+                    }`}>
                     <span className={`icon ${iconSize && "is-" + iconSize}`}>
                         <i className={`${iconGroup} fa-${icon}`} />
                     </span>
