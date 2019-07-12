af09ea272f340cfb205fb7bfa857933833fbe912
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-11 21:29:07 -0700

    Centered any modal content

--- client/src/shared/Modal.jsx
+++ client/src/shared/Modal.jsx
@@ -4,7 +4,9 @@ export default function Modal({ children, isVisible, onClose }) {
     return (
         <div className={`modal ${isVisible && "is-active"}`}>
             <div className="modal-background" onClick={onClose} />
-            <div className="modal-content">{children}</div>
+            <div className="modal-content" style={{ justifyContent: "center", display: "flex" }}>
+                {children}
+            </div>
             <button className="modal-close is-large" aria-label="close" onClick={onClose} />
         </div>
     );
