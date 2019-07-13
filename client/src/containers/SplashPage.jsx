666fa062ad07c16bc1918ba4ad16036c5ce832e0
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-13 09:00:11 -0700

    Added basic favorite page population

--- client/src/containers/SplashPage.jsx
+++ client/src/containers/SplashPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import LoginSignupModal from "../shared/LoginSignupModal";
+import SignupLoginModal from "../shared/SignupLoginModal";
 
 // TODO: Design initial index/splash page
 export default function SplashPage() {
     const [isVisible, setIsVisible] = useState(true);
     return (
         <main>
-            <LoginSignupModal isVisible={isVisible} onClose={() => setIsVisible(false)} />
+            <SignupLoginModal isVisible={isVisible} onClose={() => setIsVisible(false)} />
         </main>
     );
 }
