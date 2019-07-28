372adb3beca4b8141bfe8010c801fd2d1c80437b
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-07-27 18:29:41 -0700

    Fixed various css responsive issues

--- client/src/containers/RandomizerPage.jsx
+++ client/src/containers/RandomizerPage.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { device } from "../shared/Device";
+import React, { useState, useCallback } from "react";
 
 const basebox = {};
 const position = {};
@@ -15,180 +14,140 @@ export default function RandomizerPage(props) {
     const [shouldAnimateRotation, setShouldAnimationRotate] = useState(false);
     const [shouldChildAnimate, setShouldChildAnimationRotate] = useState(false);
 
+    const animateOnClick = useCallback(
+        (event, base) => {
+            event.preventDefault();
+            setShouldAnimationRotate(!shouldAnimateRotation);
+            setShouldChildAnimationRotate(!shouldChildAnimate);
+            setTimeout(() => props.history.push("results?base=" + base), 2000);
+        },
+        [shouldAnimateRotation, shouldChildAnimate, props.history]
+    );
+
     return (
         <main>
-            <div class="basebg" />
+            <div class="basebg">
+                <div class="column is-full is-centered has-text-centered" id="randomizertitle">
+                    <p class="title is-2">CHOOSE YOUR BASE:</p>
+                </div>
 
-            <div class="column is-full is-centered has-text-centered" id="randomizertitle">
-                <p class="title is-2">CHOOSE YOUR BASE:</p>
+                <ul class={"circleContainer" + (shouldAnimateRotation ? " rotateActive" : "")} style={position}>
+                    {/* Coffee Card */}
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "coffee")}>
+                        <img class="img" src="/image/coffee.png" alt="coffee" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            COFFEE
+                        </span>
+                    </li>
+
+                    {/* Tea card */}
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "tea")}>
+                        <img class="img" src="/image/tea.png" alt="tea" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            TEA
+                        </span>
+                    </li>
+
+                    {/* Lemonade card */}
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "lemonade")}>
+                        <img class="img" src="/image/lemonade.png" alt="lemonade" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            LEMONADE
+                        </span>
+                    </li>
+
+                    {/* Rebel card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "rebel")}>
+                        <img class="img" src="/image/rebel.png" alt="rebel" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            REBEL
+                        </span>
+                    </li>
+
+                    {/* Smoothie card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "smoothie")}>
+                        <img class="img" src="/image/smoothie.png" alt="smoothie" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            SMOOTHIE
+                        </span>
+                    </li>
+
+                    {/* Frost card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "frost")}>
+                        <img class="img" src="/image/frost.png" alt="frost" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            FROST
+                        </span>
+                    </li>
+
+                    {/* Soda card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "soda")}>
+                        <img class="img" src="/image/soda.png" alt="soda" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            SODA
+                        </span>
+                    </li>
+
+                    {/* Chai card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "chai")}>
+                        <img class="img" src="/image/chai.png" clat="chai" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            CHAI
+                        </span>
+                    </li>
+
+                    {/* Cocoa card */}
+
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "cocoa")}>
+                        <img class="img" src="/image/cocoa.png" alt="cocoa" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            COCOA
+                        </span>
+                    </li>
+                    {/* Mocha card */}
+                    <li
+                        className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
+                        style={basebox}
+                        onClick={event => animateOnClick(event, "mocha")}>
+                        <img class="img" src="/image/mocha.png" alt="mocha" style={imageSize} />
+                        <span class="title is-4 basecenter" style={center}>
+                            MOCHA
+                        </span>
+                    </li>
+                </ul>
             </div>
-
-            <ul class={"circleContainer" + (shouldAnimateRotation ? " rotateActive" : "")} style={position}>
-                {/* Coffee Card */}
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        setTimeout(() => props.history.push("results?base=coffee"), 2000);
-                    }}>
-                    <img class="img" src="/image/coffee.png" alt="coffee" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        COFFEE
-                    </span>
-                </li>
-
-                {/* Tea card */}
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=tea"), 2000);
-                    }}>
-                    <img class="img" src="/image/tea.png" alt="tea" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        TEA
-                    </span>
-                </li>
-
-                {/* Lemonade card */}
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? " rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=lemonade"), 2000);
-                    }}>
-                    <img class="img" src="/image/lemonade.png" alt="lemonade" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        LEMONADE
-                    </span>
-                </li>
-
-                {/* Rebel card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=rebel"), 2000);
-                    }}>
-                    <img class="img" src="/image/rebel.png" alt="rebel" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        REBEL
-                    </span>
-                </li>
-
-                {/* Smoothie card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=smoothie"), 2000);
-                    }}>
-                    <img class="img" src="/image/smoothie.png" alt="smoothie" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        SMOOTHIE
-                    </span>
-                </li>
-
-                {/* Frost card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=frost"), 2000);
-                    }}>
-                    <img class="img" src="/image/frost.png" alt="frost" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        FROST
-                    </span>
-                </li>
-
-                {/* Soda card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=soda"), 2000);
-                    }}>
-                    <img class="img" src="/image/soda.png" alt="soda" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        SODA
-                    </span>
-                </li>
-
-                {/* Chai card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=chai"), 2000);
-                    }}>
-                    <img class="img" src="/image/chai.png" clat="chai" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        CHAI
-                    </span>
-                </li>
-
-                {/* Cocoa card */}
-
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=cocoa"), 2000);
-                    }}>
-                    <img class="img" src="/image/cocoa.png" alt="cocoa" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        COCOA
-                    </span>
-                </li>
-                {/* Mocha card */}
-                <li
-                    className={"base box boxcard " + (shouldChildAnimate ? "rotateBox" : "")}
-                    style={basebox}
-                    onClick={event => {
-                        event.preventDefault();
-                        setShouldAnimationRotate(!shouldAnimateRotation);
-                        setShouldChildAnimationRotate(!shouldChildAnimate);
-                        // setTimeout(() => props.history.push("results?base=mocha"), 2000);
-                    }}>
-                    <img class="img" src="/image/mocha.png" alt="mocha" style={imageSize} />
-                    <span class="title is-4 basecenter" style={center}>
-                        MOCHA
-                    </span>
-                </li>
-            </ul>
         </main>
     );
 }
