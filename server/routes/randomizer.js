45386c6eeaee136338f6c171647438ef68b369c4
Obi-Juan <JPalomino09@gmail.com>
2019-08-09 10:44:48 -0600

    drink creator sturcturing

--- server/routes/randomizer.js
+++ server/routes/randomizer.js
@@ -3,6 +3,7 @@ const randomizer = require("../randomizer.js");
 // Handles the /api/randomizer GET route
 module.exports = {
     GET(request, response) {
+        console.log(request.query)
         response.send(randomizer(request.query.base));
     },
 };
